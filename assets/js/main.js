e770484f95ebe524ca6c35f6b9fc4ac423573f40
Thương Hà <you@example.com>
2022-05-27 23:53:29 +0700

    hoan thanh

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const content = document.querySelector('.main-header');
+const deleteBT = document.querySelector('.deletes');
+const cancelBT = document.querySelector('.cancel');
+const confirmBT = document.querySelector('.confirm');
+const thongbao = document.querySelector('.thongbao');
